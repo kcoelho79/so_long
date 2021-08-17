527a9310a77889964f2a5c157de5db943d34ca4a
Your Name <keber.amg79@gmail.com>
2021-08-17 16:46:11 -0300

    initilize map, windows, texture

--- inc/so_long.h
+++ inc/so_long.h
@@ -6,7 +6,7 @@
 /*   By: kde-oliv <kde-oliv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/17 09:53:05 by kde-oliv          #+#    #+#             */
-/*   Updated: 2021/08/17 14:41:18 by kde-oliv         ###   ########.fr       */
+/*   Updated: 2021/08/17 16:18:52 by kde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,30 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <mlx.h>
+# include <mlx_int.h>
 # include "libft.h"
 # include <errno.h>
 # include <fcntl.h>
+
+typedef struct s_sprite
+{
+	t_img		*player;
+	t_img		*exit;
+	t_img		*coll;
+	t_img		*wall;
+	t_img		*ground;
+}		t_sprite;
 
 typedef struct s_game
 {
 	char		*fber;
 	int			height;
 	int			width;
+	int			**map;
+	void		*mlx;
+	void		*mlx_win;
+	t_img		*mlx_img;
+	t_sprite	*sprite;
 }			t_game;
 
 #endif
--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: kde-oliv <kde-oliv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/17 09:44:04 by kde-oliv          #+#    #+#             */
-/*   Updated: 2021/08/17 14:58:34 by kde-oliv         ###   ########.fr       */
+/*   Updated: 2021/08/17 16:44:26 by kde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,13 +43,64 @@ static void	read_fber(int f, t_game *game)
 		game->fber = swp;
 		b = read(f, buff, 1);
 	}
-	// close(f);
 	free(buff);
 }
 
-void	map_init(t_game *game)
+void	texture_load(t_game *game, t_img **img, char *path)
 {
+	int	width;
+	int	height;
+
+	*img = mlx_xpm_file_to_image(game->mlx, path, &width, &height);
+	(*img)->width = width;
+	(*img)->height = height;
+}
+
+void	windows_init(t_game *game)
+{
+	game->mlx = mlx_init();
+	game->mlx_win = mlx_new_window(game->mlx, game->width * 40, game->height * 40, "So_long");
+	game->mlx_img = mlx_new_image(game->mlx, game->width * 40, game->height *40);
+	texture_load(game, &game->sprite->player, "./img/player.xpm");
+	texture_load(game, &game->sprite->exit, "./img/helicopter.xpm");
+	texture_load(game, &game->sprite->coll, "./img/gas.xpm");
+	texture_load(game, &game->sprite->wall, "./img/wall.xpm");
+	texture_load(game, &game->sprite->ground, "./img/sand.xpm");
+}
+
+void	map_init(t_game	*game)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	k = 0;
+	j = -1;
+	game->map = (int **)malloc(sizeof(int *) * game->height);
+	while (++j < game->height)
+	{
+		game->map[j] = (int *)malloc(sizeof(int) * game->width);
+		i = 0;
+		while (i < game->width)
+		{
+			game->map[j][i++] = game->fber[k++] - 48;
+		}
+		k++;
+	}
+}
+
+void	game_init(t_game *game)
+{
+	game->sprite = (t_sprite *)malloc(sizeof(t_sprite));
+	game->sprite->player = 0;
+	game->sprite->exit = 0;
+	game->sprite->coll = 0;
+	game->sprite->wall = 0;
+	game->sprite->ground = 0;
 	get_map_dimension(game);
+	map_init(game);
+	windows_init(game);
+	
 }
 
 int	main(int argc, char *argv[])
@@ -64,7 +115,9 @@ int	main(int argc, char *argv[])
 	if (f < 0)
 		perror("erro openning file");
 	read_fber(f, game);
+	close(f);
 	printf("%s\n", game->fber);
-	map_init(game);
-	printf("Dimension %iX%i\n", game->height, game->width);
+	game_init(game);
+	//render(game)
+	mlx_loop(game->mlx);
 }
